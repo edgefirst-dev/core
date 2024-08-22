87065e08de6c5f0dc15103778fd2779756c4c729
Sergio Xalambrí <hello@sergiodxa.com>
2024-08-22 15:42:45 -0500

    Split AI class run method in multiple specific methods

--- src/lib/ai.ts
+++ src/lib/ai.ts
@@ -117,64 +117,83 @@ export namespace AI {
 export class AI {
 	constructor(protected ai: Ai) {}
 
-	async run(
-		model: AI.Model.TextClassification,
-		inputs: AI.Input.TextClassification,
-		options?: AI.Options,
-	): Promise<AI.Output.TextClassification>;
-	async run(
-		model: AI.Model.ImageClassification,
-		inputs: AI.Input.ImageClassification,
-		options?: AI.Options,
-	): Promise<AI.Output.ImageClassification>;
-	async run(
+	textToImage(
 		model: AI.Model.TextToImage,
 		inputs: AI.Input.TextToImage,
 		options?: AI.Options,
-	): Promise<AI.Output.TextToImage>;
-	async run(
+	): Promise<AI.Output.TextToImage> {
+		return this.ai.run(model, inputs, options);
+	}
+
+	imageToText(
 		model: AI.Model.ImageToText,
 		inputs: AI.Input.ImageToText,
 		options?: AI.Options,
-	): Promise<AI.Output.ImageToText>;
-	async run(
+	): Promise<AI.Output.ImageToText> {
+		return this.ai.run(model, inputs, options);
+	}
+
+	translation(
 		model: AI.Model.Translation,
 		inputs: AI.Input.Translation,
 		options?: AI.Options,
-	): Promise<AI.Output.Translation>;
-	async run(
+	): Promise<AI.Output.Translation> {
+		return this.ai.run(model, inputs, options);
+	}
+
+	summarization(
 		model: AI.Model.Summarization,
 		inputs: AI.Input.Summarization,
 		options?: AI.Options,
-	): Promise<AI.Output.Summarization>;
-	async run(
+	): Promise<AI.Output.Summarization> {
+		return this.ai.run(model, inputs, options);
+	}
+
+	textEmbeddings(
 		model: AI.Model.TextEmbeddings,
 		inputs: AI.Input.TextEmbeddings,
 		options?: AI.Options,
-	): Promise<AI.Output.TextEmbeddings>;
-	async run(
+	): Promise<AI.Output.TextEmbeddings> {
+		return this.ai.run(model, inputs, options);
+	}
+
+	textGeneration(
 		model: AI.Model.TextGeneration,
 		inputs: AI.Input.TextGeneration,
 		options?: AI.Options,
-	): Promise<AI.Output.TextGeneration>;
-	async run(
+	): Promise<AI.Output.TextGeneration> {
+		return this.ai.run(model, inputs, options);
+	}
+
+	objectDetection(
 		model: AI.Model.ObjectDetection,
 		inputs: AI.Input.ObjectDetection,
 		options?: AI.Options,
-	): Promise<AI.Output.ObjectDetection>;
-	async run(
+	): Promise<AI.Output.ObjectDetection> {
+		return this.ai.run(model, inputs, options);
+	}
+
+	speechRecognition(
 		model: AI.Model.SpeechRecognition,
 		inputs: AI.Input.SpeechRecognition,
 		options?: AI.Options,
-	): Promise<AI.Output.SpeechRecognition>;
-	async run(
-		model: AI.Model,
-		inputs: AI.Input,
+	): Promise<AI.Output.SpeechRecognition> {
+		return this.ai.run(model, inputs, options);
+	}
+
+	textClassification(
+		model: AI.Model.TextClassification,
+		inputs: AI.Input.TextClassification,
 		options?: AI.Options,
-	): Promise<AI.Output> {
-		// @ts-expect-error - The Worker AI types expect a specific model, because
-		// we're using a union type, TS marks it as an error, but it's fine since we
-		// are using overloads to handle the different models, inputs and outputs
+	): Promise<AI.Output.TextClassification> {
+		return this.ai.run(model, inputs, options);
+	}
+
+	imageClassification(
+		model: AI.Model.ImageClassification,
+		inputs: AI.Input.ImageClassification,
+		options?: AI.Options,
+	): Promise<AI.Output.ImageClassification> {
 		return this.ai.run(model, inputs, options);
 	}
 }
