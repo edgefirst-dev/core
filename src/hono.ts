05375a9f3e75b51d543843cb80c40c6621810cde
Sergio Xalambrí <hello@sergiodxa.com>
2024-08-26 02:08:02 -0500

    Update Hono middleware JSDoc

--- src/hono.ts
+++ src/hono.ts
@@ -2,6 +2,17 @@ import { createMiddleware } from "hono/factory";
 import { storage } from "./lib/storage.js";
 import type { Bindings } from "./lib/types.js";
 
+/**
+ * Creates an Edge-first Runtime middleware for Hono. Any request after this
+ * will have access to the Edge environment.
+ * @returns A Hono middleware that provides access to the Edge environment.
+ * @example
+ * app.use(edgeRuntime());
+ * app.get("/", async () => {
+ *   let data = await kv().get("key");
+ *   // more code
+ * })
+ */
 export function edgeRuntime() {
 	return createMiddleware<{
 		Bindings: Bindings;
