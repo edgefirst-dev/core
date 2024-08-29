b751324b94c739d12d3d0e51253444637a3cb7d7
Sergio Xalambrí <hello@sergiodxa.com>
2024-08-29 18:20:34 -0500

    Create internal_store and unstable_orm

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import type { Request } from "@cloudflare/workers-types";
 import Headers from "@mjackson/headers";
+import type { Logger } from "drizzle-orm";
+import { drizzle } from "drizzle-orm/d1";
 import { AI } from "./lib/ai.js";
 import { Cache } from "./lib/cache.js";
 import { DB } from "./lib/db.js";
@@ -19,13 +20,18 @@ import type { Bindings } from "./lib/types.js";
 
 export type { AI, Cache, DB, FS, KV, Queue, Env };
 
+export function internal_store(key: string) {
+	let store = storage.getStore();
+	if (!store) throw new EdgeContextError(key);
+	return store;
+}
+
 /**
  * Upload, store and serve images, videos, music, documents and other
  * unstructured data in your Edge-first application.
  */
 export function fs() {
-	let c = storage.getStore();
-	if (!c) throw new EdgeContextError("fs");
+	let c = internal_store("fs");
 	if ("FS" in c.bindings) return new FS(c.bindings.FS);
 	throw new EdgeConfigError("FS");
 }
@@ -34,11 +40,8 @@ export function fs() {
  * Cache functions result in your Edge-first applications.
  */
 export function cache() {
-	let c = storage.getStore();
-	if (!c) throw new EdgeContextError("cache");
-	if ("KV" in c.bindings) {
-		return new Cache(c.bindings.KV, c.waitUntil);
-	}
+	let c = internal_store("cache");
+	if ("KV" in c.bindings) return new Cache(c.bindings.KV, c.waitUntil);
 	throw new EdgeConfigError("KV");
 }
 
@@ -47,9 +50,22 @@ export function cache() {
  * relational data.
  */
 export function db() {
-	let c = storage.getStore();
-	if (!c) throw new EdgeContextError("db");
+	let c = internal_store("db");
 	if ("DB" in c.bindings) return new DB(c.bindings.DB);
+	throw new EdgeConfigError("DB");
+}
+
+/**
+ * Get a Drizzle ORM instance for your Edge-first application already connected
+ * to your D1 database.
+ * @param schema The Drizzle schema of your database
+ * @param logger An optional custom logger
+ */
+export function unstable_orm<
+	Schema extends Record<string, unknown> = Record<string, never>,
+>(schema: Schema, logger?: Logger) {
+	let c = internal_store("db");
+	if ("DB" in c.bindings) return drizzle(c.bindings.DB, { schema, logger });
 	throw new EdgeConfigError("DB");
 }
 
@@ -65,8 +81,7 @@ export function env() {
  * application.
  */
 export function kv() {
-	let c = storage.getStore();
-	if (!c) throw new EdgeContextError("kv");
+	let c = internal_store("kv");
 	if ("KV" in c.bindings) return new KV(c.bindings.KV);
 	throw new EdgeConfigError("KV");
 }
@@ -75,8 +90,7 @@ export function kv() {
  * Access the request object in your Edge-first application.
  */
 export function request() {
-	let c = storage.getStore();
-	if (!c) throw new EdgeContextError("request");
+	let c = internal_store("request");
 	return c.request;
 }
 
@@ -85,8 +99,7 @@ export function request() {
  * application.
  */
 export function signal() {
-	let c = storage.getStore();
-	if (!c) throw new EdgeContextError("signal");
+	let c = internal_store("signal");
 	return c.request.signal;
 }
 
@@ -95,8 +108,7 @@ export function signal() {
  * @returns An `@mjackson/headers` object
  */
 export function headers() {
-	let c = storage.getStore();
-	if (!c) throw new EdgeContextError("headers");
+	let c = internal_store("headers");
 	return new Headers(c.request.headers);
 }
 
@@ -104,8 +116,7 @@ export function headers() {
  * Run machine learning models, such as LLMs in your Edge-first application.
  */
 export function unstable_ai() {
-	let c = storage.getStore();
-	if (!c) throw new EdgeContextError("ai");
+	let c = internal_store("ai");
 	if ("AI" in c.bindings) return new AI(c.bindings.AI);
 	throw new EdgeConfigError("AI");
 }
@@ -115,8 +126,7 @@ export function unstable_ai() {
  * application.
  */
 export function unstable_geo() {
-	let c = storage.getStore();
-	if (!c) throw new EdgeContextError("geo");
+	let c = internal_store("geo");
 	return new Geo(c.request);
 }
 
@@ -124,11 +134,8 @@ export function unstable_geo() {
  * Enqueue for processing later any kind of payload of data.
  */
 export function unstable_queue() {
-	let c = storage.getStore();
-	if (!c) throw new EdgeContextError("queue");
-	if ("QUEUE" in c.bindings) {
-		return new Queue(c.bindings.QUEUE, c.waitUntil);
-	}
+	let c = internal_store("queue");
+	if ("QUEUE" in c.bindings) return new Queue(c.bindings.QUEUE, c.waitUntil);
 	throw new EdgeConfigError("Queue");
 }
 
