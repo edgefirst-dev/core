7bfad2538a096dea73567295f3edd31a3d139d0a
Sergio Xalambrí <hello@sergiodxa.com>
2024-10-18 03:36:05 -0500

    Add support for queue consumers

--- src/worker.ts
+++ src/worker.ts
@@ -1,5 +1,6 @@
 import type {
 	ExportedHandler,
+	MessageBatch,
 	Request,
 	Response,
 	ScheduledController,
@@ -127,6 +128,57 @@ export function bootstrap(
 				() => handlers.onSchedule?.(event, bindings, ctx),
 			);
 		},
+
+		queue(batch: MessageBatch, bindings, ctx) {
+			if (!handlers.onQueue) {
+				throw new Error(
+					"To use queue consumers, you must provide an onQueue handler when bootstrapping your application.",
+				);
+			}
+
+			let waitUntil = ctx.waitUntil.bind(ctx);
+
+			let cache = bindings.KV ? new Cache(bindings.KV, waitUntil) : undefined;
+			let db = bindings.DB ? new DB(bindings.DB) : undefined;
+			let env = new Env(bindings);
+			let sessionStorage = bindings.KV
+				? new WorkerKVSessionStorage(bindings.KV)
+				: undefined;
+			let fs = bindings.FS ? new FS(bindings.FS) : undefined;
+			let kv = bindings.KV ? new KV(bindings.KV) : undefined;
+			let ai = bindings.AI ? new AI(bindings.AI) : undefined;
+			let queue = bindings.QUEUE
+				? new Queue(bindings.QUEUE, waitUntil)
+				: undefined;
+			let orm =
+				bindings.DB && options?.orm
+					? drizzle(bindings.DB, options.orm)
+					: undefined;
+			let rateLimit = bindings.KV
+				? new WorkerKVRateLimit(
+						bindings.KV,
+						options?.rateLimit ?? { limit: 100, period: 60 },
+					)
+				: undefined;
+
+			return storage.run(
+				{
+					ai,
+					db,
+					fs,
+					kv,
+					env,
+					orm,
+					cache,
+					queue,
+					bindings,
+					rateLimit,
+					waitUntil,
+					sessionStorage,
+				},
+				() => handlers.onQueue?.(batch, bindings, ctx),
+			);
+		},
 	};
 }
 
@@ -152,6 +204,12 @@ export namespace bootstrap {
 
 		onSchedule?(
 			event: ScheduledController,
+			bindings: Bindings,
+			ctx: ExecutionContext,
+		): Promise<void>;
+
+		onQueue?(
+			batch: MessageBatch,
 			bindings: Bindings,
 			ctx: ExecutionContext,
 		): Promise<void>;
